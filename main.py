bed678e1c84cad1e59286182bf26de920162e76c
Diamond Tusks <90947391+DiamondTusks@users.noreply.github.com>
2022-08-12 01:52:38 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 # print(type(data))
 # print(data["temp "])
 
@@ -22,9 +22,44 @@ data = pandas.read_csv("weather_data.csv")
 # average_temp = round(sum(temp_list) / len(temp_list))
 # print(average_temp)
 
-## Average temp using pandas 
+## Average temp using pandas
 # average_temp = data["temp"].mean()
 # print(average_temp)
 
 # To get max using pandas
-print(data["temp"].max())
+# print(data["temp"].max())
+
+# # Get data in columns
+# print(data["condition"])
+# print(data.condition)
+
+# Get data in row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# monday_temp = int(monday.temp)
+# monday_temp_f = (monday_temp * 1.8) + 32
+# print(monday_temp_f)
+
+# Creating dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+# data = pandas.DataFrame(data_dict)
+# print(data)
+# data.to_csv("new_data.csv")
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count, cinnamon_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
